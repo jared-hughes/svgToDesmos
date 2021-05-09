599ec7c91acc2a8763f69774b90130daed4187bf
Jared Hughes <jahughes241@gmail.com>
2021-05-09 01:42:30 -0700

    Upload and read SVG file

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,23 @@ function insertSVGAsExpressions(svg: string, normalize: boolean) {
   console.log("Done with inserting SVG");
 }
 
-(window as any).insertSVGAsExpressions = insertSVGAsExpressions;
+function injectElement() {
+  const addExpressionContainer = document.querySelector(
+    ".dcg-add-expression-container"
+  );
+  if (addExpressionContainer === null) return;
+  addExpressionContainer.insertAdjacentHTML(
+    "afterend",
+    "<input id='insertSVG' type='file' accept='.svg'/>"
+  );
+  const insertSVGElement = document.getElementById(
+    "insertSVG"
+  ) as HTMLInputElement;
+  insertSVGElement.addEventListener("change", async () => {
+    const svgString = await insertSVGElement.files?.[0]?.text();
+    if (svgString === undefined) return;
+    insertSVGAsExpressions(svgString, true);
+  });
+}
+
+injectElement();
