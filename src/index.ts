2ab07e81c78d9c0e4933b0b0265996c44c2c2bcf
Jared Hughes <jahughes241@gmail.com>
2021-10-23 17:23:05 -0700

    Integrate extactText

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,54 @@
 import { svgToExpressions } from "./svgToExpressions";
 import { insertExpressions } from "./calcHelpers";
+import extractText from "./extractText";
 
 function injectElement() {
-  const addExpressionContainer = document.querySelector(
-    ".dcg-add-expression-container"
-  );
-  if (addExpressionContainer === null) {
+  let container = document.querySelector(
+    ".dcg-header-desktop .align-right-container"
+  ) as HTMLDivElement | null;
+  if (container === null) {
     setTimeout(injectElement, 100);
     return;
   }
-  addExpressionContainer.insertAdjacentHTML(
-    "afterend",
-    "<input id='insertSVG' type='file' accept='.svg'/>"
+  const injectedContents = document.createElement("div");
+  container.prepend(injectedContents);
+  injectedContents.outerHTML = `
+    <div style='display: flex; flex-direction: column; line-height: normal'>
+      <div style="display: flex">
+        <label for='insertSVG'> SVG: </label>
+        <input id='insertSVG' type='file' accept='.svg'/>
+      </div>
+      <div style="display: flex">
+        <input id='textToExtract' type='text'/>
+        <button id='extractText'>
+          Extract
+        </button>
+      </div>
+    </div>`;
+  attachToInsertSVGElement(
+    document.getElementById("insertSVG") as HTMLInputElement
   );
-  const insertSVGElement = document.getElementById(
-    "insertSVG"
-  ) as HTMLInputElement;
+  attachToExtractTextButton(
+    document.getElementById("extractText") as HTMLButtonElement
+  );
+  container.style.display = "flex";
+}
+
+function attachToInsertSVGElement(insertSVGElement: HTMLInputElement) {
   (window as any).svgToDesmosInputElement = insertSVGElement;
   insertSVGElement.addEventListener("change", async () => {
     const file = insertSVGElement.files?.[0];
     if (file === undefined) return;
     const svgString = await file.text();
     insertExpressions(svgToExpressions(svgString, file.name));
+  });
+}
+
+function attachToExtractTextButton(extractTextButton: HTMLButtonElement) {
+  extractTextButton.addEventListener("click", () => {
+    const text = (document.getElementById("textToExtract") as HTMLInputElement)
+      .value;
+    extractText(text);
   });
 }
 
